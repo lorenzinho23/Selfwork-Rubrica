08ecd4469f9533424f46eac53ced82e88bdc2927
Lorenzo <lorenzo_tomasello@virgilio.it>
2026-05-16 13:03:59 +0200

    Selfwork Rubrica

---
+++ script.js
@@ -0,0 +1,80 @@
+let nameInput = document.getElementById("nameInput");
+let phoneInput = document.getElementById("phoneInput");
+
+let showButton = document.getElementById("view-contacts");
+let addButton = document.getElementById("add-contact");
+let removeButton = document.getElementById("remove-contact");
+let editButton = document.getElementById("edit-contact");
+
+let check = false;
+
+let contactsList = document.querySelector(".containerContacts");
+
+let rubrica = {
+  listaContatti: [
+    { name: "Lorenzo", phone: "1234567890" },
+    { name: "Riccardo", phone: "0987654321" },
+  ],
+
+  showcontacts: function () {
+    this.listaContatti.forEach((contatto) => {
+      let p = document.createElement("p");
+      p.innerHTML = `${contatto.name}: ${contatto.phone}`;
+      contactsList.appendChild(p);
+    });
+  },
+
+  addcontact: function (newName, newPhone) {
+    this.listaContatti.push({ name: newName, phone: newPhone });
+  },
+
+  removecontact: function (removeName) {
+    let filtered = this.listaContatti.filter(
+      (contatto) => contatto.name != removeName,
+    );
+    this.listaContatti = filtered;
+  },
+
+  editcontact: function (editName, newPhone) {
+    this.listaContatti.forEach((contatto) => {
+      if (contatto.name == editName) {
+        contatto.phone = newPhone;
+      }
+    });
+  },
+};
+
+showButton.addEventListener("click", () => {
+  if (check == false) {
+    rubrica.showcontacts();
+    showButton.innerHTML = "Nascondi contatti";
+    check = true;
+  } else {
+    check = false;
+    contactsList.innerHTML = "";
+    showButton.innerHTML = "Mostra contatti";
+  }
+});
+
+addButton.addEventListener("click", () => {
+  if (nameInput.value != "" && phoneInput.value != "") {
+    rubrica.addcontact(nameInput.value, phoneInput.value);
+    nameInput.value = "";
+    phoneInput.value = "";
+  }
+});
+
+removeButton.addEventListener("click", () => {
+  if (nameInput.value != "") {
+    rubrica.removecontact(nameInput.value);
+    nameInput.value = "";
+  }
+});
+
+editButton.addEventListener("click", () => {
+  if (nameInput != "") {
+    rubrica.editcontact(nameInput.value, phoneInput.value);
+    nameInput.value = "";
+    phoneInput.value = "";
+  }
+});
